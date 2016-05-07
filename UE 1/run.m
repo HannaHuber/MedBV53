4079546bdd8719561a571a10f449604c2d60160d
HannaHuber <hanna_huber@hotmail.com>
2016-05-07 16:58:16 +0200

    add covariances (1b)  to run.m (weird plots?)

--- UE 1/run.m
+++ UE 1/run.m
@@ -1,3 +1,32 @@
+% *** MedBV UE1 - Group 53 ***
+
+%% Ex. 1 - Covariance
+clear all
+close all
+
+% Load data
+load daten
+
+% Calculate covariance matrices for all data sets
+C1 = ourCov(data1);
+C2 = ourCov(data2);
+C3 = ourCov(data3);
+C4 = ourCov(data4);
+
+% Plot results
+figure()
+plot(C1);
+axis equal;
+figure()
+plot(C2);
+axis equal;
+figure()
+plot(C3);
+axis equal;
+figure()
+plot(C4);
+axis equal;
+
 %% Ex. 2 - PCA
 clear all
 close all
