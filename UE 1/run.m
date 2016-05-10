8f8998ddc763e2774def28742cd0868057544cd7
HannaHuber <hanna_huber@hotmail.com>
2016-05-10 13:04:44 +0200

    implement reconstruction (nebenvector incorrect?)

--- UE 1/run.m
+++ UE 1/run.m
@@ -4,7 +4,7 @@
 clear all
 close all
 
-% Load data
+% Load 2D data
 load daten
 
 % Calculate covariance matrices for all data sets
@@ -13,23 +13,23 @@ C2 = ourCov(data2);
 C3 = ourCov(data3);
 C4 = ourCov(data4);
 
-% Plot results
-figure()
-% plot(C1,'.');
-plot(data1(1,:),data1(2,:),'.');
-axis equal;
-figure()
-% plot(C2,'.');
-plot(data2(1,:),data2(2,:),'.');
-axis equal;
-figure()
-% plot(C3,'.');
-plot(data3(1,:),data3(2,:),'.');
-axis equal;
-figure()
-% plot(C4,'.');
-plot(data4(1,:),data4(2,:),'.');
-axis equal;
+% % Plot results
+% figure()
+% % plot(C1,'.');
+% plot(data1(1,:),data1(2,:),'.');
+% axis equal;
+% figure()
+% % plot(C2,'.');
+% plot(data2(1,:),data2(2,:),'.');
+% axis equal;
+% figure()
+% % plot(C3,'.');
+% plot(data3(1,:),data3(2,:),'.');
+% axis equal;
+% figure()
+% % plot(C4,'.');
+% plot(data4(1,:),data4(2,:),'.');
+% axis equal;
 
 %% Ex. 2 - PCA
 
@@ -42,16 +42,26 @@ plot2DPCA(data3', mean(data3,2)', data3', E3, l3, 1, 0)
 [E4,l4] = pca(data4);
 plot2DPCA(data4', mean(data4,2)', data4', E4, l4, 1, 0)
 
-%% Ex. 3 - Subspace projection
+%% Ex. 3 - Subspace projection (2D -> 1D)
 
-% Project data3 to main vector (data are now 1D)
-p3Main = project(data3, E3(:,1));
-plot2DPCA(p3Main', mean(p3Main, 2)', p3Main', E3, l3, 1, 0)
+% Project + reconstruct data3 to main vector
+[p3Main, errMain] = project(data3, E3, 1);
+plot2DPCA(data3', mean(p3Main, 2)', p3Main', E3, l3, 1, 1)
 
-% 2D project to second vector
-p3Second = project(data3, E3(:,2));
+% Project + reconstruct data3 to second vector
+[p3Second, errSecond] = project(data3, E3, 2);
+plot2DPCA(data3', mean(p3Second, 2)', p3Second', E3, l3, 1, 1)
+
+%% Ex. 4 - Subspace projection (3D -> 2D)
+
+clear all
+close all
+
+% Load 3D data
+load daten3d
 
 % 3D project to two main vectors
+
 %% Ex. 5 - Shape Modell
 clear all 
 close all
