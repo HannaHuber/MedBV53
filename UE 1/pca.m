7a09c94cdfde4ffa863deb36caf32a211e94a794
LillyTraudl <ltmeg@hotmail.de>
2016-05-10 12:05:46 +0200

    pca
    lambda wird immer als (spalten)vektor ausgegeben (egal welcher Dimension
    die Daten sind)

--- UE 1/pca.m
+++ UE 1/pca.m
@@ -4,13 +4,12 @@ function [ E, lambda ] = pca( D, percentage )
 %   n ... number of data points
 %   E contains the Eigenvectors to the corresponding eigenvalues of lambda
 %   (which are in decending order)
+%
 % percentage is not mandatory; if given the highest eigenvalues are given
 % back such that percentage of the variance is at least within these
 % principal components
 
-% Step 1: Compute the empirical mean & subtract it from the data
-%D_mm = D - ones(size(D,1),1)*mean(D);
-% empirical covariance matrix
+% Step 1: Compute the empirical mean & subtract it from the data and calculate empirical covariance matrix
 C_D = ourCov(D);
 
 % Step 2: Compute eigenvalue decomposition of the covariance matrix
@@ -20,30 +19,21 @@ lambda = diag(lambda);
 % diag matrix
 E = fliplr(E);
 lambda = flipud(lambda);
-if size(D,2)==3
-        lambda = diag(lambda);
-end
 
 %% if only the highest eigenvalues shall be taken according to percentage
 if nargin == 2
-    if size(D,2)==2
-        lambda = diag(lambda);
-    end
     %calculate the total variance
-    lambda_tot = sum(sum(lambda));
+    lambda_tot = sum(lambda);
     %find how many principal components are necessary
     k = 0;
     percentage_mom = 0;
     while percentage_mom < percentage
         k = k+1;
-        percentage_mom = percentage_mom + lambda(k,k)/lambda_tot;
+        percentage_mom = percentage_mom + lambda(k)/lambda_tot;
     end
     % prepare the output
     E = E(:,1:k);
-    lambda = lambda(1:k,1:k);
-    if size(D,2)==2
-        lambda = diag(lambda);
-    end
+    lambda = lambda(1:k);
 end
 end
 
