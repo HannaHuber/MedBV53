fa721b7a16017acacf8bdf97f26141b4f3f0c6e3
HannaHuber <hanna_huber@hotmail.com>
2016-06-08 18:10:05 +0200

    change vec2mat to reshape in computeFeatures

--- UE 2/computeFeatures.m
+++ UE 2/computeFeatures.m
@@ -6,32 +6,35 @@ function [ imageFeatures ] = computeFeatures( image )
 % get size in pixels
 dim=size(image);
 
+% convert image to double (necessary for Haarlike)
+image=im2double(image);
+
 % Init feature matrix
 imageFeatures=zeros(44,dim(1)*dim(2));
 
-%greyvalue
+% greyvalue
 imageVec=image(:)';
 imageFeatures(1,:)=imageVec;
 
-%gradient
-[grad1a,grad2a]=gradient(im2double(image));
+% gradient
+[grad1a,grad2a]=gradient(image);
 grad1=grad1a(:)';
 grad2=grad2a(:)';
 imageFeatures(2,:)=grad1;
 imageFeatures(3,:)=grad2;
 
-%magnitude of the gradient
+% magnitude of the gradient
 maggrad=sqrt(grad1.*grad1+grad2.*grad2);
 imageFeatures(4,:)=maggrad;
 
-%Haar-like features of the gray value image
+% Haar-like features of the gray value image
 imageFeatures(5:24,:)=computeHaarLike(image);
 
-%Haar-like features of the gradient magnitudes
-maggradMatrix=vec2mat(maggrad,dim(1))';
+% Haar-like features of the gradient magnitudes
+maggradMatrix= reshape(maggrad,dim(1), dim(2)); % vec2mat(maggrad,dim(1))';
 imageFeatures(25:44,:)=computeHaarLike(maggradMatrix);
 
-%x- and y- coordinates of a pixel
+% x- and y- coordinates of a pixel
 dimensionVectorXshort=1:dim(1);
 dimensionVectorX=repmat(dimensionVectorXshort,1,dim(2));
 imageFeatures(45,:)=dimensionVectorX;
